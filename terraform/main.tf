85b9322d475de00be8bb82dd3179276500245056
Adminkeyan <kaishwarthi0321@gmail.com>
2025-05-26 22:43:29 +0530

    Update main.tf
    Creating Aws ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,46 @@
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
 
+resource "aws_instance" "web" {
+  ami                         = "ami-0c02fb55956c7d316" # Ubuntu 22.04 LTS in us-east-1
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = file("${path.module}/user_data.sh")
+
+  tags = {
+    Name = "jiitak-devops-app"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
