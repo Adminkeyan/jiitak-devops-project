c5b2be7bd66df419459c927cddb8a32431120f82
Adminkeyan <kaishwarthi0321@gmail.com>
2025-05-26 22:44:03 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,9 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "key_name" {
+  description = "EC2 Key Pair name (must already exist)"
+}
